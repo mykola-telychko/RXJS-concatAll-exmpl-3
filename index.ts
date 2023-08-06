a0cfb8d5997c67cdc75c4a66b2642508bc659d87
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-06 05:29:02 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import { interval, of } from 'rxjs';
 import { take, concatAll } from 'rxjs/operators';
 
-const inputObj = {
+// https://www.learnrxjs.io/learn-rxjs/operators/combination/concatall
+
+const inpObj = {
   o1: { iTime: 1000, qtyOutput: 5 },
   o2: { iTime: 500, qtyOutput: 2 },
   o3: { iTime: 1000, qtyOutput: 1 },
@@ -13,14 +15,18 @@ const inputObj = {
 // const obs3 = interval(2000).pipe(take(1));
 
 // new
-const obs1 = interval(1000).pipe(take(5));
-const obs2 = interval(500).pipe(take(2));
-const obs3 = interval(2000).pipe(take(1));
+const obs1 = interval(inpObj.o1.iTime).pipe(take(inpObj.o1.qtyOutput)); // 5 -> 0,1,2,3,4
+
+const obs2 = interval(inpObj.o2.iTime).pipe(take(inpObj.o2.qtyOutput)); // 2 -> 0,1
+
+const obs3 = interval(inpObj.o3.iTime).pipe(take(inpObj.o3.qtyOutput)); // 1 -> 0
+
+// explenation
 
 //emit three observables
-const source = of(obs1, obs2, obs3);
+const src = of(obs1, obs2, obs3);
 //subscribe to each inner observable in order when previous completes
-const example = source.pipe(concatAll());
+const example = src.pipe(concatAll());
 /*
   output: 0,1,2,3,4,0,1,0
   How it works...
